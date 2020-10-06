00bf688f5db5749e854bb35903940acae06b89a6
Ryan Voots <simcop2387@simcop2387.info>
2020-10-05 17:10:49 -0700

    Now moved around properly

--- lib/Device/Chip/Adapter/LinuxKernel.pm
+++ lib/Device/Chip/Adapter/LinuxKernel.pm
@@ -100,46 +100,6 @@ sub shutdown {
 }
 
 package
-   Device::Chip::Adapter::LinuxKernel::_base;
-
-use Carp;
-
-sub new {
-   my $class = shift;
-
-   bless { }, $class;
-}
-
-# Most modes have no GPIO on this system
-sub list_gpios { return qw( ) }
-
-sub write_gpios {
-   my $self = shift;
-   my ( $gpios ) = @_;
-
-   foreach my $pin ( keys %$gpios ) {
-         croak "Unrecognised GPIO pin name $pin";
-   }
-}
-
-sub read_gpios {
-   my $self = shift;
-   my ( $gpios ) = @_;
-
-   my @f;
-   foreach my $pin ( @$gpios ) {
-     croak "Unrecognised GPIO pin name $pin";
-   }
-}
-
-# there's no more efficient way to tris_gpios than just read and ignore the result
-sub tris_gpios
-{
-   my $self = shift;
-   $self->read_gpios->then_done();
-}
-
-package
     Device::Chip::Adapter::LinuxKernel::_GPIO;
     
 use base qw( Device::Chip::Adapter::LinuxKernel::_base );
